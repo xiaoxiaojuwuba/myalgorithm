df4ca3dc2efbc55c1062968e9ad8e7cb6827e0b9
yuanshunx <shunxiyn@cn.ibm.com>
2022-02-16 17:31:13 +0900

    init

---
+++ src/main/java/myalgorithm/App.java
@@ -0,0 +1,13 @@
+package myalgorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
